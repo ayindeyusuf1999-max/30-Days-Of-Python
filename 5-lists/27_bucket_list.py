bf5e6547e31018f8d3e515d5f4b82ec79197cb1c
ayindeyusuf1999 <ayindeyusuf1999@gmail.com>
2026-05-21 12:10:41 -0400

    Initial commit -Python 101

---
+++ 5-lists/27_bucket_list.py
@@ -0,0 +1,14 @@
+# Bucket List 🪣
+# Codédex
+
+things_to_do = [
+  '🚀 Build a meaningful product for everyone.',
+  '⛰ Try out hiking and mountain biking.',
+  '🌏 Visit at least 10 countries in my lifetime.',
+  '🎸 Produce an original song.',
+  '📝 Write a short story.',
+  '🏃 Finish a 10k marathon.'
+]
+
+for thing in things_to_do:
+  print(thing)
